be0d3853bf886c0c4da3de91e7c8f5c293ce6269
Aman Singh Chandel <amanchandel4@gmail.com>
2023-02-13 19:24:29 +0530

    requirements updates

--- bazaarApp/api/urls.py
+++ bazaarApp/api/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 router = routers.DefaultRouter()
 router.register(r'data', views.BazarViewSet)
-router.register(r'agent', views.BazarAgentViewSet)
-router.register(r'wholeseller', views.BazarWholesellerViewSet)
-router.register(r'product', views.BazarProductViewSet)
+# router.register(r'agent', views.BazarAgentViewSet, 'agent-list')
+# router.register(r'wholeseller', views.BazarWholesellerViewSet, 'wholeseller-list')
+# router.register(r'product', views.BazarProductViewSet, 'product-list')
 
 
 # Wire up our API using automatic URL routing.
--- mwb/settings.py
+++ mwb/settings.py
@@ -175,8 +175,8 @@ ADMIN_SITE_HEADER = "mwb"
 ...
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=90),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=95),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
     'UPDATE_LAST_LOGIN': False,
@@ -202,8 +202,8 @@ SIMPLE_JWT = {
     'JTI_CLAIM': 'jti',
 
     'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
-    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
-    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+    'SLIDING_TOKEN_LIFETIME': timedelta(days=90),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=95),
 }
 
 
