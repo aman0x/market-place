815c61c72f124b08419817c1ed20adc19e0434b9
ravi-cloudastra <ravisingh@cloudastra.in>
2023-03-06 18:40:34 +0530

    locationApp

--- locationApp/models.py
+++ locationApp/models.py
@@ -4,9 +4,17 @@ from django.db import models
 class State(models.Model):
     state=models.CharField(max_length=30,default=None)
 
-class City(models.Model):
-    city=models.CharField(max_length=30,null=True,default=None)
-
 class District(models.Model):
     district=models.CharField(max_length=30,null=True,default=None)
+    state=models.ForeignKey(
+        State, on_delete=models.CASCADE, related_name='state_district')
+    
+    def __str__(self):
+        return self.state
 
+class City(models.Model):
+    city=models.CharField(max_length=30,null=True,default=None)
+    state=models.ForeignKey(
+        State, on_delete=models.CASCADE, related_name='state_district_city')
+    district=models.ForeignKey(
+        District, on_delete=models.CASCADE, related_name='district_city')
