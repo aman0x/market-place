d73fc9e054570ed321934c6b2ba98ebb5e7a58a3
ravi-cloudastra <ravisingh@cloudastra.in>
2023-03-17 13:52:35 +0530

    filter updated

--- parentCategoryApp/api/serializers.py
+++ parentCategoryApp/api/serializers.py
@@ -6,3 +6,8 @@ class ParentCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = ParentCategory
         fields = '__all__'
+
+class ParentCategoryFilterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ParentCategory
+        fields = '__all__'
