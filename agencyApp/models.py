0ae446f7f812c304b1d7d1c44f6890a71daa66df
ravi-cloudastra <ravisingh@cloudastra.in>
2023-03-22 03:25:34 +0530

    agency model added district

--- agencyApp/models.py
+++ agencyApp/models.py
@@ -8,6 +8,7 @@ class Agency(models.Model):
     address=models.CharField(max_length=50,default=None,blank=True)
     landmark=models.CharField(max_length=50,default=None,blank=True)
     state=models.ManyToManyField(State,related_name="agency_state")
+    district=models.ManyToManyField(District,related_name="agency_district")
     city=models.ManyToManyField(City,related_name="agency_city")
     pin_code=models.CharField(max_length=20,default=None,blank=True)
     gst_image=models.ImageField(upload_to="image/agency/",null=True)
