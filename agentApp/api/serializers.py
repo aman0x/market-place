e7a1023d823fb6f8f4437622e9b5c0b67560b577
Aman Singh Chandel <amanchandel4@gmail.com>
2023-03-27 15:38:11 +0530

    support for base64 update

--- agentApp/api/serializers.py
+++ agentApp/api/serializers.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 from agentApp.models import Agent, ManageCommision
-from bazaarApp.models import Bazaar
-from agencyApp.models import Agency
+from drf_extra_fields.fields import Base64ImageField
+
 
 
 class AgentManageCommisionSerializer(serializers.ModelSerializer):
@@ -13,7 +13,24 @@ class AgentManageCommisionSerializer(serializers.ModelSerializer):
 
 
 class AgentSerializer(serializers.ModelSerializer):
+    agent_image = Base64ImageField(required=False)
+    agent_adhar_front_image = Base64ImageField(required=False)
+    agent_adhar_back_image = Base64ImageField(required=False)
+    product_upload_mrp_label_image = Base64ImageField(required=False)
+    
     class Meta:
         model = Agent
         fields = '__all__'
+    
+    def update(self, instance, validated_data):
+        instance.agent_image = validated_data.get(
+            'agent_image')
+        instance.agent_adhar_front_image = validated_data.get(
+            'agent_adhar_front_image')
+        instance.agent_adhar_back_image = validated_data.get(
+            'agent_adhar_back_image')
+        instance.agent_pancard_image = validated_data.get(
+            'agent_pancard_image')
+        event = super().update(instance, validated_data)
+        return event
 
