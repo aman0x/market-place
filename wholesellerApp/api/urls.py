e360d5005d7f3c05434d64a4cc4e27317111326d
ravi-cloudastra <ravisingh@cloudastra.in>
2023-04-18 16:04:03 +0530

    agent wallet

--- wholesellerApp/api/urls.py
+++ wholesellerApp/api/urls.py
@@ -3,12 +3,16 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'', views.WholesellerViewSet)
+router.register(r'data', views.WholesellerViewSet),
+# router.register(r'dash', views.WholesellerDashboardViewSet),
+
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
+    path('dashboard/', views.WholesellerDashboardViewSet.as_view({'get': 'list'}),name="Wholeseller-dashboard"),
+    path('dashboard/bazaar/', views.WholesellerDashboardBazzarViewSet.as_view({'get': 'list'}),name="Wholeseller-dashboard"),
 ]
 
-
+urlpatterns += router.urls
