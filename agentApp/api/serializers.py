c22bd006851b723536fbb5a39851c4abf1961965
Aman Singh Chandel <amanchandel4@gmail.com>
2023-04-24 18:09:39 +0530

    bazaar data + agent state

--- agentApp/api/serializers.py
+++ agentApp/api/serializers.py
@@ -6,6 +6,8 @@ from wholesellerApp.models import Wholeseller
 from agencyApp.models import Agency
 from bazaarApp.models import Bazaar
 from bazaarApp.api.serializers import BazaarSerializer
+from locationApp.api.serializers import *
+from locationApp.models import *
 
 class AgentManageCommisionSerializer(serializers.ModelSerializer):
     class Meta:
@@ -29,6 +31,12 @@ class AgentSerializer(serializers.ModelSerializer):
     agent_pancard_image = Base64ImageField(required=False)
     agent_agency_name = serializers.SerializerMethodField()
     agent_bazaar_data = serializers.SerializerMethodField()
+    agent_assigned_state_names = serializers.SerializerMethodField()
+    agent_assigned_district_names = serializers.SerializerMethodField()
+    agent_assigned_city_names = serializers.SerializerMethodField()
+    agent_city = serializers.SerializerMethodField()
+    agent_district = serializers.SerializerMethodField()
+    agent_state = serializers.SerializerMethodField()
     
     
     
@@ -43,12 +51,56 @@ class AgentSerializer(serializers.ModelSerializer):
             firm_name = Agency.objects.filter(id=agency_id).get().firm_name
         return firm_name
     
+    def get_agent_state(self, obj):
+        state = ""
+        state_id = obj.agent_state_id
+        if state is not None:
+            state = State.objects.filter(id=state_id).get().state
+        return state
+        
+        
+    def get_agent_district(self, obj):
+        district = ""
+        district_id = obj.agent_district_id
+        if district is not None:
+            district = District.objects.filter(id=district_id).get().district
+        return district
+
+    
+    def get_agent_city(self, obj):
+        city = ""
+        city_id = obj.agent_city_id
+        if city is not None:
+            city = City.objects.filter(id=city_id).get().city
+        return city
+        
+
+    
     def get_agent_bazaar_data(self, obj):
         bazaar_ids = obj.agent_bazaar.all()
         bazaar = Bazaar.objects.filter(id__in=bazaar_ids)
         serializer = BazaarSerializer(bazaar, many=True)
         return serializer.data
 
+    def get_agent_assigned_state_names(self, obj):
+        state_ids = obj.agent_assigned_state.all()
+        states = State.objects.filter(id__in=state_ids)
+        serializer = StateSerializer(states, many=True)
+        return serializer.data
+
+    def get_agent_assigned_district_names(self, obj):
+        district_ids = obj.agent_assigned_district.all()
+        district = District.objects.filter(id__in=district_ids)
+        serializer = DistrictSerializer(district, many=True)
+        return serializer.data
+
+    def get_agent_assigned_city_names(self, obj):
+        city_ids = obj.agent_assigned_city.all()
+        city = City.objects.filter(id__in=city_ids)
+        serializer = CitySerializer(city, many=True)
+        return serializer.data
+    
+    
     def update(self, instance, validated_data):
         instance.agent_image = validated_data.get(
             'agent_image')
