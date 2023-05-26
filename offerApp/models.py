d1a37a3f78ddfe1ab3446a8aa194cae8d2d2dedc
ravi-cloudastra <ravisingh@cloudastra.in>
2023-05-26 12:22:28 -0700

    wholeseller offer app model modified

--- offerApp/models.py
+++ offerApp/models.py
@@ -6,20 +6,30 @@ from datetime import datetime
 from django.db import models
 from subCategoryApp.models import SubCategory, Category, ParentCategory, Bazaar
 from productApp.models import Product
+from masterApp.models import RetailerType
+from retailerApp.models import Retailer
 from wholesellerApp.models import Wholeseller, Agent
+
 
 DISCOUNT_BY_TYPE = (
     ("PERCENTAGE", "Percentage"),
     ("AMOUNT", "Amount")
 )
 
+CREATE_OFFER_BY_TYPE = (
+    ("GROUP", "Group"),
+    ("CATEGORY", "Category"),
+    ("SUBCATEGORY", "SubCategory"),
+    ("PRODUCT", "Item")
+)
 
 class Offers(models.Model):
-    category_group = models.ForeignKey(ParentCategory, on_delete=models.CASCADE,
+    create_offer_by_item = models.CharField(max_length=20, choices=CREATE_OFFER_BY_TYPE, default="GROUP")
+    category_group = models.ForeignKey(ParentCategory, on_delete=models.CASCADE,null=True,
                                        related_name='offer_product_parent_category')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='offer_product_category')
-    subcategory = models.ForeignKey(SubCategory, on_delete=models.CASCADE, related_name='offer_product_subcategory')
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='offer_product_name')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,null=True, related_name='offer_product_category')
+    subcategory = models.ForeignKey(SubCategory, on_delete=models.CASCADE,null=True, related_name='offer_product_subcategory')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE,null=True, related_name='offer_product_name')
 
     offer_base_price = models.DecimalField(decimal_places=2, max_digits=12, null=True)
     offer_discount_value_type = models.CharField(max_length=20,
@@ -27,7 +37,7 @@ class Offers(models.Model):
                                                  default="PERCENTAGE")
     offer_discount_value = models.CharField(max_length=20, null=True, default=None)
     offer_discounted_price = models.CharField(max_length=20, null=True, default=None)
-    offer_coupon_code = models.CharField(max_length=200)
+    #offer_coupon_code = models.CharField(max_length=200)
     offer_start_date = models.DateField(default=datetime.now, blank=True)
     offer_end_date = models.DateField(default=datetime.now, blank=True)
     offer_min_qty = models.IntegerField()
@@ -39,6 +49,11 @@ class Offers(models.Model):
     offer_image = models.ImageField(upload_to="offer-image/offer/%y/%m/%d", null=True)
     offer_active = models.BooleanField(default=True)
     offer_for = models.CharField(max_length=200)
+    customer_type = models.ForeignKey(RetailerType, on_delete=models.CASCADE, null=True,
+                                       related_name='offer_customer_type')
+    customer = models.ForeignKey(Retailer, on_delete=models.CASCADE, null=True,
+                                       related_name='offer_customer')
+
 
     class Meta:
         verbose_name_plural = "offers"
