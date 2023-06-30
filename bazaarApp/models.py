0fc9f09c758d160487715a39b4b62ea3b7ecb0ad
aman0x <amanchandel4@gmail.com>
2023-06-30 12:57:35 +0530

    format model

--- bazaarApp/models.py
+++ bazaarApp/models.py
@@ -7,23 +7,15 @@ from masterApp.models import RetailerType
 class Bazaar(models.Model):
     bazaar_name = models.CharField(max_length=200, null=True)
     bazaar_description = models.TextField(blank=True, null=True)
-    bazaar_image = models.ImageField(
-        upload_to='image/bazaar/%Y/%m/%d')
-    bazaar_state = models.ManyToManyField(
-        State, related_name='bazaar_state')
-    bazaar_district = models.ManyToManyField(
-        District, related_name='bazaar_district')
-    bazaar_city = models.ManyToManyField(
-        City, related_name='bazaar_city')
-    bazaar_added_date = models.DateTimeField(
-        default=datetime.now, blank=True)
-    bazaar_updated_date = models.DateTimeField(
-        default=datetime.now, blank=True)
-    bazaar_updated_by = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='bazaar_updated_by', null=True)
+    bazaar_image = models.ImageField(upload_to='image/bazaar/%Y/%m/%d')
+    bazaar_state = models.ManyToManyField(State, related_name='bazaar_state')
+    bazaar_district = models.ManyToManyField(District, related_name='bazaar_district')
+    bazaar_city = models.ManyToManyField(City, related_name='bazaar_city')
+    bazaar_added_date = models.DateTimeField(default=datetime.now, blank=True)
+    bazaar_updated_date = models.DateTimeField(default=datetime.now, blank=True)
+    bazaar_updated_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bazaar_updated_by', null=True)
     bazaar_steps = models.IntegerField(null=True, default=None)
-    bazaar_retailer_type = models.ManyToManyField(
-        RetailerType, related_name="bazaar_retailer_type", blank=True)
+    bazaar_retailer_type = models.ManyToManyField(RetailerType, related_name="bazaar_retailer_type", blank=True)
     bazaar_active = models.BooleanField(default=True)
 
     class Meta:
