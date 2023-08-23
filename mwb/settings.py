1f17ecc0ecae2c68c60a1c5a48aefd6a2d45b89d
aman0x <amanchandel4@gmail.com>
2023-08-23 12:47:50 +0530

    change pgination settings

--- mwb/settings.py
+++ mwb/settings.py
@@ -68,7 +68,7 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 1000,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
